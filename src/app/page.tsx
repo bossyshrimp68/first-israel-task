9d88763bb6f47072fb226c1215b89836b29c20e9
bossyshrimp68 <romypine007@gmail.com>
2023-05-08 19:24:03 +0300

    Romy - went to eat, doesnt add 20:(

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+"use client"; 
+import React, { useState } from "react";
 
-export default function Home() {
+
+
+
+export default function form() {
+  const [state, setState] = useState({
+    counter: 0
+  });
+
+  function onSub(){
+    console.log(state.counter)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-      <p>Welcome! Start editing your code in the src/app folder and don{"'"}t forget to write a README!</p>
-      </div>
-    </main>
+    <div>
+      <form onSubmit={onSub}>
+        <p>בדיקת ציוד</p>
+        <input type='checkbox' value={state.counter+20}></input>
+
+        <button type="submit">submit</button>
+      </form>
+    </div>
   )
 }
